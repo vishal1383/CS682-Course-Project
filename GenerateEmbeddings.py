57addbb39ea950a2426053696d1dd639dcfa6d1d
Sai Sreenivas <chinthasaisreenivas@gmail.com>
2024-10-13 18:02:37 -0400

    add text processor & generate embeddings

---
+++ GenerateEmbeddings.py
@@ -0,0 +1,71 @@
+import json
+import numpy as np
+import torch
+from PIL import Image
+from transformers import CLIPProcessor, CLIPModel
+from TextPreprocessor import TextPreprocessor
+
+ROOT_EMBEDDINGS_FOLDER = './Embeddings'
+
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, (np.ndarray, torch.Tensor)):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+class GenerateEmbeddings:
+    def __init__(self, data_path, root_embeddings_path = ROOT_EMBEDDINGS_FOLDER):
+        self.data_path = data_path
+        self.text_preprocessor = TextPreprocessor()
+        self.model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+        self.processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+
+        self.root_embeddings_path = root_embeddings_path
+
+    def generate_embeddings(self, texts, images):
+        preprocessed_texts = []
+        for text in texts:
+            preprocessed_texts.append(self.text_preprocessor.preprocess_text(text))
+        
+        inputs = self.processor(preprocessed_texts, images, return_tensors = "pt", truncation = True)
+        outputs = self.model(**inputs)
+        return [outputs['text_embeds'], outputs['image_embeds'], outputs.logits_per_image]
+
+    # Generate embedding for the image data
+    def generate_image_embedding(self, img):
+        inputs = self.processor(images = Image.open(img), return_tensors = "np")
+        outputs = self.model(**inputs)
+        return outputs['image_embeds']
+
+    # Generate embedding for the text data
+    def generate_text_embedding(self, text):
+        preprocessed_texts = []
+        for text in texts:
+            preprocessed_texts.append(self.text_preprocessor.preprocess_text(text))
+        
+        inputs = self.processor(text = preprocessed_texts, images = None, return_tensors = "np")
+        outputs = self.model(**inputs)
+        return outputs['text_embeds']
+
+    # Save generated embeddings dictionary to a json file
+    def save_embeddings(self, embeddings, embeddings_path):
+        json_data = json.dumps(embeddings, cls = NumpyEncoder)
+        with open(embeddings_path, 'w') as file:
+            file.write(json_data)
+
+if __name__ == '__main__':
+    generateEmbeddings = GenerateEmbeddings(data_path ='./destination')
+    imgs = [Image.open('BatBall.jpeg')]
+    texts = ["Bat and Ball", "I am not the right text"]
+    outputs = generateEmbeddings.generate_embeddings(texts, images = imgs)
+    
+    # Note: We might have to normalize the embeddings
+    # Access text embeddings using outputs['text_embeds]
+    # Access image embeddings using outputs['image_embeds]
+
+    # Save embeddings using save_embeddings
+    
+    # Calculate the image-text similarity score
+    logits_per_image = outputs[2]
+    probs = logits_per_image.softmax(dim = 1)
+    print(probs)
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # CS682-Course-Project
 This is a initial repo for course project of CS682
+
+# Installation Steps
+
+- Create a conda environment and activate it
+- Use the following commands to install relevant packages:
+
+```
+- conda install numpy
+- conda install anaconda::pillow
+- conda install pytorch::pytorch
+- conda install conda-forge::transformers
+```
