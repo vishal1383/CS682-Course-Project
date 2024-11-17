4cb884ed8e466671adf3fe3aa6e26d594e365b62
Sai Sreenivas <chinthasaisreenivas@gmail.com>
2024-11-16 23:59:18 -0500

    add draft code for custom nn

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cs682-course-project",
+    version="0.1",
+    packages=find_packages(exclude=["datasets", "docs", "embeddings", "retrieved_items", "static"]),
+)
